6d1d79a98f843a6b17272a5620af4e59393579d4
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2021-11-29 03:50:39 -0600

    Update connection.js
    Connecting to Cloud DB

--- Server/connection.js
+++ Server/connection.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql");
 const db = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password:'Kaushik@1',
-    database: 'triviaking',
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
    });
    
    db.connect(function(err){
