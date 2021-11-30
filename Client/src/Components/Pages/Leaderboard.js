773968afdc84b7df6a7fd6efba6f6a765b4222a0
harshadeep97 <Chharshadeep@gmail.com>
2021-11-30 11:38:50 +0530

    Added search feature on the leaderboard table

--- Client/src/Components/Pages/Leaderboard.js
+++ Client/src/Components/Pages/Leaderboard.js
@@ -1,72 +1,77 @@
-import { Component } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Axios from 'axios';
 import { Container,Row,Table} from 'react-bootstrap';
 import {Redirect} from 'react-router-dom';
 import  Navbar  from "../Navbar";
+import Search from "../../Table/Search";
 
-class Leaderboard extends Component{
-    constructor(props)
-    {
-        super(props);
-        const token = localStorage.getItem('token');
-        let loggedIn=true;
-        if(token==null){
-            loggedIn=false
-        }
-        
-        this.state = {
-            Data:[],
-            loggedIn,
-            count:0
-        }
-    }
-    componentDidMount()
-    {
+function Leaderboard()
+{
+    const [token,setToken]=useState(localStorage.getItem("token"));
+    const [search, setSearch] = useState("");
+    const [Data,setData] = useState([]);
+   
+    useEffect ( () => { 
         var url='http://localhost:3002/leaderboard/';
         Axios.get(url).then(res => {
            const a=JSON.parse(res.data)
-           this.setState({Data:a})
+           setData(a)
         })
-    }
-    render(props){
-        if(this.state.loggedIn===false){
+    },[])
+
+    const SearchedData = useMemo(() => {
+        let a = Data;
+
+        if (search) {
+            a = a.filter(
+                field =>
+                    field.username.toLowerCase().includes(search.toLowerCase()) ||
+                    field.category.toLowerCase().includes(search.toLowerCase())
+            );
+        }
+        return a;
+    }, [Data,  search]);
+
+        if(token===null)
+        {
             return <Redirect to="/"/>
-          }
+        }
+   
         return(
-            <Container fluid className="fontClass">
+           
+        <Container fluid className="fontClass">
             <Row>
-            < Navbar/>
+               < Navbar/>
             </Row>
-            <Row>
-            <Table striped bordered hover>
+            <Row>    
+                <div className="col-md-6"  style={{margin: 10 +'px'}}>
+                  <Search onSearch={value => { setSearch(value); }} />
+                </div>
+                <Table striped bordered hover>
             <thead>
-             <tr>
-            <th>#</th>
-            <th>Username</th>
-            <th>Category</th>
-            <th>Score</th>
-            </tr>
+                <tr>
+                  <th>Username</th>
+                  <th>Category</th>
+                  <th>Score</th>
+                </tr>
             </thead>
-                <tbody>
-                    {
-                        this.state.Data.map( data => 
-                            (
-                                <>
-                                <tr>
-                                    <td>{++this.state.count}</td>
-                                    <td>{data.username}</td>
-                                    <td>{data.category}</td>
-                                    <td>{data.score}</td> 
-                                  </tr>
-                                  </>
-                        ))
-                    }
-                </tbody>
-            </Table>
-            </Row>
-            </Container>
-        )
-    }
+            <tbody>
+                 {
+                   SearchedData.map( data => 
+                        (
+                          <>
+                            <tr>
+                            <td>{data.username}</td>
+                            <td>{data.category}</td>
+                            <td>{data.score}</td>       
+                            </tr>
+                          </>
+                        ))}
+            </tbody>
+            </Table>        
+        </Row>
+    </Container>
+        ) 
 }
 
 export default Leaderboard;
